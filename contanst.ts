5fd0c6fddf4f637cd2f97c9ead1d265dbc43eca1
jesus <jesus_lopez1997@hotmail.com>
2023-10-09 11:31:18 -0400

    add env files

--- contanst.ts
+++ contanst.ts
@@ -1,8 +1,17 @@
-export const API_URL = "http://192.168.100.98/api";
-// export const API_URL = "http://192.168.100.15/api";
+export const API_URL: string =
+  process.env.EXPO_PUBLIC_API_URL || "http://localhost/api";
 
-export const PUSHER_HOST ="192.168.100.98";
-export const PUSHER_KEY_APP = "app-key";
-export const PUSHER_PORT = 6001;
-export const PUSHER_SCHEME = "http";
-export const PUSHER_APP_CLUSTER = "mt1";
+export const PUSHER_HOST: string =
+  process.env.EXPO_PUBLIC_PUSHER_HOST || "http://localhost";
+
+export const PUSHER_KEY_APP: string =
+  process.env.EXPO_PUBLIC_PUSHER_KEY_APP || "app-key";
+
+export const PUSHER_PORT: number =
+  Number(process.env.EXPO_PUBLIC_PUSHER_PORT) || 6001;
+
+export const PUSHER_SCHEME: string = 
+  process.env.EXPO_PUBLIC_PUSHER_SCHEME || "http";
+
+export const PUSHER_APP_CLUSTER: string = 
+  process.env.EXPO_PUBLIC_PUSHER_APP_CLUSTER || "mt1";
